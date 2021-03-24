f7ae4333f177e12411800749372bee7f3bdea951
Arga2108 <77953606+Arga2108@users.noreply.github.com>
2021-03-24 08:31:07 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -16,10 +16,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'Kirisaki',
+            namaowner: 'Arga',
+            instagram: 'arga.nicoy',
+            youtube : 'Arga Bot Wa'
         }
     }
     res.json(config)
